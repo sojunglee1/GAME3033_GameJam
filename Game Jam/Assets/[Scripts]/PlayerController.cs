86bca4397b156646f2a94c8f7f9086c48a74a039
Sojung Lee <sojung.lee2@georgebrown.ca>
2022-02-25 21:32:54 -0500

    Added Basic Enemy

--- Game Jam/Assets/[Scripts]/PlayerController.cs
+++ Game Jam/Assets/[Scripts]/PlayerController.cs
@@ -9,7 +9,6 @@ public class PlayerController : MonoBehaviour
     public GameObject followTransform;
 
     [SerializeField] float walkSpeed = 5;
-    [SerializeField] float runSpeed = 10;
     [SerializeField] float jumpForce = 5;
 
     Vector2 inputVector = Vector2.zero;
@@ -31,21 +30,17 @@ public class PlayerController : MonoBehaviour
     {
         followTransform.transform.rotation *= Quaternion.AngleAxis(lookInput.x * aimSensitivity, Vector3.up);
 
-        followTransform.transform.rotation *= Quaternion.AngleAxis(lookInput.y * aimSensitivity, Vector3.left);
-
         var angles = followTransform.transform.localEulerAngles;
 
         //rotate the player to face where we are looking
-        transform.rotation = Quaternion.Euler(0, followTransform.transform.rotation.eulerAngles.y, 0);
         followTransform.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
 
         if (isJumping) return;
         if (!(inputVector.magnitude > 0)) moveDirection = Vector3.zero;
 
         moveDirection = transform.forward * inputVector.y + transform.right * inputVector.x;
-        float currentSpeed = isRunning ? runSpeed : walkSpeed;
 
-        Vector3 movementDirection = moveDirection * (currentSpeed * Time.deltaTime);
+        Vector3 movementDirection = moveDirection * (walkSpeed * Time.deltaTime);
 
         transform.position += movementDirection;
 
